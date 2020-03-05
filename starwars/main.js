6bb75cd285f52d33f47612dc12c7be6d26874622
Colt Hedin <shatsky95@yahoo.com>
2020-03-04 19:36:58 -0700

    worked on buttons

---
+++ starwars/main.js
@@ -0,0 +1,77 @@
+import { films } from '../starwars/data/films.js'
+import { people} from '../starwars/data/people.js'
+import { starships} from '../starwars/data/starships.js'
+
+
+const greetingDiv = document.querySelector('.greeting')
+
+const maleButton = document.querySelector('#maleButton')
+const femaleButton = document.querySelector('#femaleButton')
+const otherButton = document.querySelector('#otherButton')
+
+
+const  otherCharacters = people.filter(person => {
+    if(person.gender === "hermaphrodite"  
+    || person.gender === "n/a"
+    || person.gender === "none") {
+        return person
+    }
+})
+
+    
+
+    maleButton.addEventListener("click", (event) => {
+        populateDOM(people.filter(person => person.gender === "male"))
+    })
+    femaleButton.addEventListener("click", (event) => {
+            populateDOM(people.filter(person => person.gender === "female"))
+    })       
+    otherButton.addEventListener("click", (event) => {
+                populateDOM(otherCharacters)    
+    })
+        //"url: "https://swapi.co/api/people/10/"
+
+        function getCharNumber(url){
+            let end = url.lastIndexOf('/')
+            let start = end - 2
+            if(url.charAt(start) === '/'){
+                start++
+            }
+            return url.slice(start, end)
+        }
+
+        //getCharNumber("https://swapi.co/api/people/1/")
+        
+
+    
+
+function populateDOM(Characters) {
+Characters.forEach(person => {
+    //need to extract the number from the person url property
+    let charNum = getCharNumber(person.url)
+    
+    let anchorWrap = document.createElement("a")
+    anchorWrap.href = "#"
+
+    let imageItem = document.createElement("img")
+    imageItem.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+
+    //imageItem.addEventListener('error', (event) => {
+        //console.log(`${event.type}: Loading image\n`)
+        //console.log(event)
+        //imageItem.hidden = true }
+    
+    
+    // add some way to handle user clicks on the image
+    imageItem.addEventListener("click", () => {
+        console.log("IT worked")
+    })
+    anchorWrap.appendChild(imageItem)
+    greetingDiv.appendChild(anchorWrap)
+    
+    
+
+})
+
+}
+
