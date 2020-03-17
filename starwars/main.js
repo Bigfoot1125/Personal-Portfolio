e828370a92d82910920f20f8df030c105f5d0bbc
Colt Hedin <colthedin@gmail.com>
2020-03-16 21:49:41 -0600

    updated and finished up page

--- starwars/main.js
+++ starwars/main.js
@@ -1,71 +1,56 @@
-import { people } from '../starwars/data/people.js'
-import { getLastNumber, removeChildren} from 'utils.js'
-
-
-
+import { people } from '../data/people.js'
+import { getLastNumber, removeChildren } from '../scripts/utils.js'
+ 
 const gallery = document.querySelector('.gallery')
-
 const maleButton = document.querySelector('#maleButton')
 const femaleButton = document.querySelector('#femaleButton')
 const otherButton = document.querySelector('#otherButton')
-
-
+ 
 const otherCharacters = people.filter(person => {
-    if (person.gender === "hermaphrodite"
-        || person.gender === "n/a"
-        || person.gender === "none") {
-        return person
-    }
+   if (
+      person.gender === "hermaphrodite"
+   || person.gender === "n/a"
+   || person.gender === "none"
+) {
+       return person
+   }
 })
-
-
-
-maleButton.addEventListener("click", (event) => {
-    populateDOM(people.filter(person => person.gender === "male"))
+ 
+maleButton.addEventListener('click', event => {
+   populateDOM(people.filter(person => person.gender === "male"))
 })
-femaleButton.addEventListener("click", (event) => {
-    populateDOM(people.filter(person => person.gender === "female"))
+ 
+femaleButton.addEventListener("click", event => {
+   populateDOM(people.filter(person => person.gender === "female"))
 })
-otherButton.addEventListener("click", (event) => {
-    populateDOM(otherCharacters)
+ 
+otherButton.addEventListener("click", event => {
+   populateDOM(otherCharacters)
 })
-
-
-
-
-
-
-
-
-
-function populateDOM(Characters) {
-    removeChildren(gallery)
-    Characters.forEach(person => {
-        //need to extract the number from the person url property
-        let charNum = getLastNumber(person.url)
-
-        let anchorWrap = document.createElement("a")
-        anchorWrap.href = "#"
-
-        let imageItem = document.createElement("img")
-        imageItem.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
-
-        //imageItem.addEventListener('error', (event) => {
-        //console.log(`${event.type}: Loading image\n`)
-        //console.log(event)
-        //imageItem.hidden = true }
-
-
-        // add some way to handle user clicks on the image
-        imageItem.addEventListener("click", event => {
-            console.log(event)
-        })
-        anchorWrap.appendChild(imageItem)
-        gallery.appendChild(anchorWrap)
-
-
-
-    })
-
+ 
+function populateDOM(characters) {
+   removeChildren(gallery)
+   characters.forEach(person => {
+   //need to extract the number from the person.url property
+   let charNum = getLastNumber(person.url)
+   let anchorWrap = document.createElement('a')
+   anchorWrap.href = '#'
+ 
+   let imageItem = document.createElement('img')
+   imageItem.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+  
+   imageItem.addEventListener('error', event => {
+       //console.log(`${event.type}: Loading image\n`)
+       //console.log(event)
+       imageItem.hidden = true
+       //imageItem.src = '../images/uvu-logo.jpg'
+   })
+ 
+   // add some ways to handle user clicks on the image
+   imageItem.addEventListener('click' , event => {
+       console.log(event)
+   })
+   anchorWrap.appendChild(imageItem)
+   gallery.appendChild(anchorWrap)
+  })
 }
-
