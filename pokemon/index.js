3c2323be09ce199aadca10accfc25ad896cdc03c
Colt Hedin <colthedin@gmail.com>
2020-04-10 20:17:47 -0600

    worked on my pokemon page

--- pokemon/index.js
+++ pokemon/index.js
@@ -1,17 +1,57 @@
 function getPokeData(url) {
-    let pokemonGrid = document.querySelector('.pokemonGrid')
-    fetch(url).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data)
-            
-            //pokemonGrid.textContent = data.name
+
+  fetch(url).then(function (response) {
+    response.json().then(function (pokeData) {
+      console.log(pokeData.results)
+      const pokeMap = pokeData.results.map(pokemon => {
+        return fetch(pokemon.url).then(resData => {
+          resData.json().then(pokeJson =>{
+            console.log(pokeJson)
+            return pokeJson.height
           })
+
+
+        
+          
+         // return resData.height
+        })
       })
+
+
+     // console.log(pokeMap)
+     // populatePokeCards(pokeData.results)
+
+
+
+    })
+  })
 }
 
-getPokeData('https://pokeapi.co/api/v2/pokemon/1/') 
+let pokemonGrid = document.querySelector('.pokemonGrid')
 
-var card = document.querySelector('.card');
-card.addEventListener( 'click', function() {
-  card.classList.toggle('is-flipped');
-}); 
+getPokeData('https://pokeapi.co/api/v2/pokemon?&limit=25')
+
+
+function populatePokeCards(pokeArray) {
+  pokeArray.forEach(pokemon => {
+    let pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    let pokeCard = document.createElement('div')
+    pokeCard.className = 'card'
+    pokeCard.addEventListener('click', () => pokeCard.classList.toggle('is-flipped'))
+
+    let pokeFront = document.createElement('div')
+    pokeFront.className = 'card__face card__face--front'
+    pokeFront.textContent = pokemon.name
+    let pokeBack = document.createElement('div')
+    pokeBack.className = 'card__face card__face--back'
+    pokeBack.textContent = 'back'
+
+    pokeCard.appendChild(pokeFront)
+    pokeCard.appendChild(pokeBack)
+    pokeScene.appendChild(pokeCard)
+    pokemonGrid.appendChild(pokeScene)
+  })
+}
+
+
