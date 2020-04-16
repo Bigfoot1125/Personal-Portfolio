befae1713434f317d9e6dfde7542e6e0c379cca0
Colt Hedin <colthedin@gmail.com>
2020-04-15 20:07:26 -0600

    worked on pokemon page

--- pokemon/index.js
+++ pokemon/index.js
@@ -9,15 +9,31 @@ async function getAPIData(url) {
     console.error(error)
   }
 }
+
 // now, use the async getAPIData function
-getAPIData('https://pokeapi.co/api/v2/pokemon/?&limit=25').then((data) => {
-  for (const pokemon of data.results) {
-    getAPIData(pokemon.url).then((pokeData) => {
-      populatePokeCard(pokeData)
-    })
-  }
-})
+
+function loadPage() {
+  getAPIData('https://pokeapi.co/api/v2/pokemon/?&limit=25').then((data) => {
+    for (const pokemon of data.results) {
+      getAPIData(pokemon.url).then((pokeData) => {
+        populatePokeCard(pokeData)
+      })
+    }
+  })
+}
+
 let pokemonGrid = document.querySelector('.pokemonGrid')
+let startButton = document.querySelector('#startButton')
+let newButton = document.querySelector('#newButton')
+
+startButton.addEventListener('click', () => {
+  loadPage()
+})
+
+newButton.addEventListener('click', () => {
+  addPokemon()
+})
+
 function populatePokeCard(singlePokemon) {
     let pokeScene = document.createElement('div')
     pokeScene.className = 'scene'
@@ -26,24 +42,36 @@ function populatePokeCard(singlePokemon) {
     pokeCard.addEventListener('click', () =>
       pokeCard.classList.toggle('is-flipped'),
     )
-    
     let pokeFront = populateCardFront(singlePokemon)
     let pokeBack = populateCardBack(singlePokemon)
-
     pokeCard.appendChild(pokeFront)
     pokeCard.appendChild(pokeBack)
     pokeScene.appendChild(pokeCard)
     pokemonGrid.appendChild(pokeScene)
 }
-
 function populateCardFront(pokemon) {
   let cardFront = document.createElement('div')
   cardFront.className = 'card__face card__face--front'
-  cardFront.textContent = pokemon.name
+  
   let frontImage = document.createElement('img')
-  frontImage.src = `../images/${pokemon.id}.png`
+  
+  frontImage.src = `../images/${getImageFileName(pokemon)}.png`
+
+  let frontLabel = document.createElement('p')
+  frontLabel.textContent = `${pokemon.name.charAt(0).toUpperCase()}${pokemon.name.slice(1)}`
   cardFront.appendChild(frontImage)
+  cardFront.appendChild(frontLabel)
   return cardFront
+}
+
+function getImageFileName(pokemon) {
+  if (pokemon.id < 10) {
+    return `00${pokemon.id}`
+  } else if (pokemon.id > 9 && pokemon.id < 100) {
+    return `0${pokemon.id}`
+  } else if (pokemon.id > 809) {
+    return `pokeball`
+  }
 }
 
 function populateCardBack(pokemon) {
@@ -57,5 +85,37 @@ function populateCardBack(pokemon) {
   })
   cardBack.appendChild(abilityList)
   return cardBack
+}
+
+class Pokemon {
+  constructor(height, weight, name, abilities) {
+    this.height = height
+    this.weight = weight
+    this.name = name
+    this.abilities = abilities
+    this.id = 900
+  }
+}
+
+function addPokemon() {
+  let Thoremon = new Pokemon(190, 290, 'thoremon',
+    [
+      {
+        ability: {
+        name: 'Thunder Belly'
+        }
+      },
+      {
+        ability: {
+        name: 'Beard Power'
+        }
+      },
+      {
+        ability: {
+        name: 'Rancid Stench'
+        }
+      }
+  ])
+  populatePokeCard(Thoremon)
 }
 
